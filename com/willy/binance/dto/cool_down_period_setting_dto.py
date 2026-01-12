d9786f06503e22c4db0b3fcbc7c48912df39143f
TW-IT-BE-Willy <willy.cheng@shoalter.com>
2026-01-12 10:29:03 +0800

    fix CoolDownPeriodSettingDto field type

--- com/willy/binance/dto/cool_down_period_setting_dto.py
+++ com/willy/binance/dto/cool_down_period_setting_dto.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 
 
 @dataclass
 class CoolDownPeriodSettingDto:
     loss_count: int
     cool_down_period: int
-    next_trade_time: datetime = None
-    last_loss_time: datetime = None
+    next_trade_time: Optional[datetime] = None
+    last_loss_time: Optional[datetime] = None
